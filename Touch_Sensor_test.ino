b9c1023e1528820181ff944f929ebcc8c5db0795
Matthew Wagner <43178844+mw354017@users.noreply.github.com>
2018-11-13 11:29:35 -0500

    Add files via upload

--- Touch_Sensor_test.ino
+++ Touch_Sensor_test.ino
@@ -1,8 +1,7 @@
 /*****************************************************
  * This file is to test the touch sensors using the serial monitor built into the Arduino IDE.
+ * This test case will alow us to analyze what values are given with touching the probes in various ways.
  ****************************************************/
-
-/// The 7 bit number that midi uses for Control Change.
 
 void setup() 
 {
@@ -13,22 +12,19 @@ void setup()
 // the loop routine runs over and over again forever:
 void loop() 
 {
-
+  //Read from each touch sensor and print the value
   Serial.print(touchRead(A9));
-    Serial.println();
+  Serial.println();
 
   Serial.print(touchRead(A8)); 
   Serial.println();
 
   Serial.print(touchRead(A5));
-    Serial.println();
+  Serial.println();
 
   Serial.print(touchRead(A4));
-
-  /// Optionally prints the value that would be sent over MIDI Control Change.
-  //Serial.print(ccValue);
+  Serial.println(); 
   Serial.println();
-    Serial.println();
 
   /// delay 100 milliseconds (1/10 of a second)
   delay(100);
