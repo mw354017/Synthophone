3e6467ac893d4b0e5caa1a292757ebffb085c770
Thomas Waldick <17waldth@mgail.com>
2018-11-19 18:34:14 -0500

    Added Unity Project and built a test exe

---
+++ GUI_Unity/Assets/Arduino.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using UnityEngine.UI;
+
+public class Arduino : MonoBehaviour {
+
+	SerialPort stream = new SerialPort("COM3", 9600);
+
+	public Text currentNoteDisplay;
+
+	string currentNote = "";
+	// Use this for initialization
+	void Start () {
+		stream.Open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string currentNote = stream.ReadLine();
+		currentNoteDisplay.text = currentNote;
+	}
+}
