41e34ecd713fc055a1fca424c8c84840e1248817
Matthew Wagner <43178844+mw354017@users.noreply.github.com>
2018-12-03 22:50:13 -0500

    transpose added

--- Synthophone.ino
+++ Synthophone.ino
@@ -16,9 +16,14 @@ int velocity=100;
 int currentState;
 int ccValue;
 int keySetup=sax;
+int transpose=0;
+int temp;
+
+String currentNoteLetter = "C#";
 
 void getNote();
-void sendData(String type, int data);
+void sendInt(String type, int data);
+void sendString(String type, String data);
 
 #define SERIAL_USB
 
@@ -36,15 +41,20 @@ void setup()
 
 void loop() 
 {
+  temp = Serial.read();
+  if(temp!=-1)
+  {
+    transpose = temp;
+  }
   breathValue = analogRead(A0);
-  sendData("breath ", breathValue);
+  sendInt("breath ", breathValue);
   if(currentState==NOTE_OFF)
   {
     if(breathValue > breathThreshold)
     {
       getNote();
       usbMIDI.sendNoteOn(currentNote, velocity, midiChannel);
-      sendData("note ", currentNote);
+      sendString("note ", currentNoteLetter);
       currentState=NOTE_ON;
     }
   }
@@ -53,6 +63,7 @@ void loop()
     if(breathValue > breathThreshold)
     {
       getNote();
+      sendString("note ", currentNoteLetter);
       //usbMIDI.sendNoteOn(currentNote, velocity, midiChannel);
       ccValue=map(breathValue,breathThreshold, maxBreath,0,127);
       usbMIDI.sendControlChange(7,ccValue,midiChannel);
@@ -68,8 +79,7 @@ void loop()
 void getNote() 
 {
   // This function reads from our 8 switches and returns a midi note value.
-  #define BASE 60
-  
+  int BASE=60;
   std::array<bool, 8> key {0,0,0,0,0,0,0,0};
   for(int i=0; i<8; i++)
   {
@@ -87,63 +97,83 @@ void getNote()
   std::array<bool, 8> E {0,0,1,1,0,1,1,1};
   std::array<bool, 8> Eb {1,1,1,1,0,1,1,1};
   std::array<bool, 8> D {0,1,1,1,0,1,1,1};
-
+ 
+ BASE+=transpose;
   if(key==Cs)
   {
     currentNote=BASE; //C#
+    currentNoteLetter = "C#";
   }
   else if(key==C)
   {
     currentNote=BASE - 1; //C
+    currentNoteLetter = "C";
   }
   else if(key==B)
   {
     currentNote=BASE - 2; //B
+    currentNoteLetter = "B";
   }
   else if(key==Bb)
   {
     currentNote=BASE - 3; //Bb
+    currentNoteLetter = "Bb";
   }
   else if(key==A)
   {
     currentNote=BASE - 4; //A
+    currentNoteLetter = "A";
   }
   else if(key==Gs)
   {
     currentNote=BASE - 5; //G#
+    currentNoteLetter = "A#";
   }
   else if(key==G)
   {
     currentNote=BASE - 6; //G
+    currentNoteLetter = "G";
   }
   else if(key==Fs)
   {
     currentNote=BASE - 7; //F#
+    currentNoteLetter = "F#";
   }
   else if(key==F)
   {
     currentNote=BASE - 8; //F
+    currentNoteLetter = "F";
   }
   else if(key==E)
   {
     currentNote=BASE - 9; //E
+    currentNoteLetter = "E";
   }
   else if(key==Eb)
   {
     currentNote=BASE - 10; //D#
+    currentNoteLetter = "D#";
   }
   else if(key==D)
   {
     currentNote=BASE - 11; //D
+    currentNoteLetter = "D";
   }
   else
   {
     
   }
+ 
 
 }
 
-void sendData(String type, int data){
+void sendInt(String type, int data){
+    Serial.print(type);
+    Serial.println(data); // need a end-line because wrmlh.csharp use readLine method to receive data
+}
+
+
+void sendString(String type, String data){
     Serial.print(type);
     Serial.println(data); // need a end-line because wrmlh.csharp use readLine method to receive data
 }
